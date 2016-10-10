3062479d79e3cd339bda84e60edb88786719295c
Vineet <vineet.iitd@gmail.com>
2016-10-10 16:30:25 +0530

    module4-assignment harder

--- module4-assignment/script.js
+++ module4-assignment/script.js
@@ -33,7 +33,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function (window){
+(function (){
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
@@ -67,4 +67,4 @@ for (i = 0; i < names.length; i++/* fill in parts of the 'for' loop to loop over
   }
 }
 
-})(window);
+})();
